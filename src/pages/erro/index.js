4a092b92d092ca1eea63fb7daa3f5070ec551ade
unknown <abreusense@gmail.com>
2022-12-16 09:49:07 -0300

    apagando dicas do index.html e ajeitando o link da página de erro

--- src/pages/erro/index.js
+++ src/pages/erro/index.js
@@ -1,16 +1,15 @@
 import './erro.css'
+import { Link } from 'react-router-dom'
 
 export default function Erro(){
     return(
         <div className='error-content'>
             <div className='title erro'>
                 <h1>
-                    <a href='/'>
-                        Iformulas
-                    </a>
+                    <Link to={'/'}>Iformulas</Link>
                 </h1>
 
-                <div className='information-erro'>
+                <div className='box-erro'>
                     <h2>Página não encontrada!</h2>
                     <p>Por favor, clique no título acima, ou digite na barra de navegação: '/'.
                         Assim Poderá voltar para o menu principal. Obrigado!
